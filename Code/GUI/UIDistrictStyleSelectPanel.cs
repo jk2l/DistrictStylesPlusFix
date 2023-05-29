3004072f8d7536aba7b52f1c5df9d2732e3b4070
Jacky Leung <jacky0002@hotmail.com>
2023-05-29 19:28:16 +1200

    Add support of copy existing style into new style

--- Code/GUI/UIDistrictStyleSelectPanel.cs
+++ Code/GUI/UIDistrictStyleSelectPanel.cs
@@ -11,6 +11,7 @@ namespace DistrictStylesPlus.Code.GUI
         private UIFastList _districtStyleSelect;
         private UIButton _addStyle;
         private UIButton _removeStyle;
+        private UIButton _copyStyle;
         internal static DistrictStyle SelectedDistrictStyle;
         
         public static UIDistrictStyleSelectPanel Instance { get; private set; }
@@ -24,6 +25,7 @@ namespace DistrictStylesPlus.Code.GUI
             SetupDistrictStyleSelect();
             SetupAddStyleButton();
             SetupRemoveStyleButton();
+            SetupCopyStyleButton();
         }
         
         internal void RefreshDistrictStyleSelect()
@@ -34,8 +36,8 @@ namespace DistrictStylesPlus.Code.GUI
         private void SetupAddStyleButton()
         {
             _addStyle = UIUtils.CreateButton(this);
-            _addStyle.width = (width - Spacing) / 2;
-            _addStyle.text = "New Style";
+            _addStyle.width = (width - Spacing) / 3;
+            _addStyle.text = "New";
             _addStyle.relativePosition = new Vector3(0, _districtStyleSelect.height + Spacing);
 
             _addStyle.eventClick += (c, p) =>
@@ -48,11 +50,10 @@ namespace DistrictStylesPlus.Code.GUI
         private void SetupRemoveStyleButton()
         {
             _removeStyle = UIUtils.CreateButton(this);
-            _removeStyle.width = (width - Spacing) / 2;
-            _removeStyle.text = "Delete Style";
+            _removeStyle.width = (width - Spacing) / 3;
+            _removeStyle.text = "Delete";
             _removeStyle.isEnabled = false;
-            _removeStyle.relativePosition = new Vector3(width - _removeStyle.width,
-                _districtStyleSelect.height + Spacing);
+            _removeStyle.relativePosition = new Vector3(_addStyle.width + Spacing/2, _districtStyleSelect.height + Spacing);
 
             _removeStyle.eventClick += (component, param) =>
             {
@@ -66,6 +67,20 @@ namespace DistrictStylesPlus.Code.GUI
                         RefreshDistrictStyleSelect();
                         component.isEnabled = false;
                     });
+            };
+        }
+
+        private void SetupCopyStyleButton()
+        {
+            _copyStyle = UIUtils.CreateButton(this);
+            _copyStyle.width = (width - Spacing) / 3;
+            _copyStyle.text = "Copy";
+            _copyStyle.relativePosition = new Vector3(width - _copyStyle.width, _districtStyleSelect.height + Spacing);
+
+            _copyStyle.eventClick += (c, p) =>
+            {
+                UIView.PushModal(UICopyStyleModal.Instance);
+                UICopyStyleModal.Instance.Show(true);
             };
         }
 
